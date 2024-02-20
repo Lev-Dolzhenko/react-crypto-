26f542ef707b92560b099762d390e9ef12c6907c
Leo <workspace2707@yandex.ru>
2024-02-20 19:21:38 +0300

    created auth

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Account from "./routes/Account";
 import { Footer } from "./components/Footer";
 import { CoinPage } from "./components/CoinPage";
 import axios from "axios";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -19,23 +20,24 @@ function App() {
   useEffect(() => {
     axios.get(url).then((responce) => {
       setCoins(responce.data);
-      // console.log(responce.data);
     });
   }, [url]);
 
   return (
     <ThemeProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home coins={coins}/>} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/account" element={<Account />} />
-        <Route path='/coin/:coinId' element={<CoinPage />}>
-          <Route path=':coinId'/>
-        </Route>
-      </Routes>
-      <Footer />
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ThemeToggle } from "./ThemeToggle";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
 
   const handleNav = () => {
     setNav(!nav);
+  };
+
+  const handleSighOut = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
@@ -18,17 +30,26 @@ function Navbar() {
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div className="hidden md:block">
-        <Link className="p-4 hover:text-accent" to="/signin">
-          Sign In
-        </Link>
-        <Link
-          className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
-          to="/signun"
-        >
-          Sign Up
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account" className="p-4">
+            Account
+          </Link>
+          <button onClick={handleSighOut}> Sign out </button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link className="p-4 hover:text-accent" to="/signin">
+            Sign In
+          </Link>
+          <Link
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
       {/* Menu icon */}
       <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -42,24 +63,27 @@ function Navbar() {
         }
       >
         <ul className="w-full p-4">
-          <li className="border-b py-6">
+          <li onClick={handleNav} className="border-b py-6">
             <Link to="/">Home</Link>
           </li>
-          <li className="border-b py-6">
-            <Link to="/">Account</Link>
+          <li onClick={handleNav} className="border-b py-6">
+            <Link to="/account">Account</Link>
           </li>
           <li className="py-6">
             <ThemeToggle />
           </li>
         </ul>
         <div className="flex flex-col w-full p-4">
-          <Link to="/signin">
+          <Link onClick={handleNav} to="/signin">
             <button className="w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl">
               Sign In
             </button>
           </Link>
           <Link to="/signup">
-            <button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+            <button
+              onClick={handleNav}
+              className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl"
+            >
               Sign Up
             </button>
           </Link>
